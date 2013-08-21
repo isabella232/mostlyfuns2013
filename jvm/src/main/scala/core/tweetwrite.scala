22bacebd78959e40a7e26dbed66e7e55769f7ac3
Jan Machacek <jan.machacek@gmail.com>
2013-08-21 14:56:20 +0100

    Writing tweets

--- jvm/src/main/scala/core/tweetwrite.scala
+++ jvm/src/main/scala/core/tweetwrite.scala
@@ -1,5 +1,18 @@
 package core
 
 import akka.actor.Actor
-import com.datastax.driver.core.{BoundStatement, Cluster}
-import scala.util.Success
+import com.datastax.driver.core.Cluster
+import domain.Tweet
+
+class TweetWriterActor(cluster: Cluster) extends Actor {
+  val session = cluster.connect(Keyspaces.akkaCassandra)
+  val preparedStatement = session.prepare("INSERT INTO tweets(key, user_user, text, createdat) VALUES (?, ?, ?, ?);")
+
+  def saveTweet(tweet: Tweet): Unit =
+    session.executeAsync(preparedStatement.bind(tweet.id.id, tweet.user.user, tweet.text.text, tweet.createdAt))
+
+  def receive: Receive = {
+    case tweets: List[Tweet] => tweets.foreach(saveTweet)
+    case tweet: Tweet        => saveTweet(tweet)
+  }
+}
