a25c8074c9d3729a9840cfe4945050aeabd6919f
Jan Machacek <jan.machacek@gmail.com>
2013-08-21 14:58:06 +0100

    Main II

--- jvm/src/main/scala/core/Main.scala
+++ jvm/src/main/scala/core/Main.scala
@@ -8,9 +8,12 @@ object Main extends App with ConfigCassandraCluster {
   import Commands._
   import akka.actor.ActorDSL._
 
+  def twitterSearchProxy(query: String) = s"http://twitter-search-proxy.herokuapp.com/search/tweets?q=$query"
+
   implicit lazy val system = ActorSystem()
   val write = system.actorOf(Props(new TweetWriterActor(cluster)))
   val read = system.actorOf(Props(new TweetReaderActor(cluster)))
+  val scan = system.actorOf(Props(new TweetScannerActor(write, twitterSearchProxy)))
   implicit val _ = actor(new Act {
     become {
       case x => println(">>> " + x)
@@ -21,6 +24,8 @@ object Main extends App with ConfigCassandraCluster {
   private def commandLoop(): Unit = {
     Console.readLine() match {
       case QuitCommand                => return
+
+      case ScanCommand(query)         => scan ! query
 
       case ListCommand(count)         => read ! FindAll(count.toInt)
       case CountCommand               => read ! CountAll
