74c0be7d88c2044720a070160d618a683c6fb7b9
Jan Machacek <jan.machacek@gmail.com>
2013-08-21 14:56:34 +0100

    Reading tweets

--- jvm/src/main/scala/core/tweetread.scala
+++ jvm/src/main/scala/core/tweetread.scala
@@ -1,6 +1,37 @@
 package core
 
 import akka.actor.Actor
-import com.datastax.driver.core.{ResultSet, BoundStatement, Cluster, Row}
+import com.datastax.driver.core.{Cluster, Row}
 import com.datastax.driver.core.querybuilder.QueryBuilder
+import core.TweetReaderActor.{FindAll, CountAll}
+import domain.Tweet
 
+
+object TweetReaderActor {
+  case object CountAll
+  case class FindAll(maximum: Int)
+}
+
+class TweetReaderActor(cluster: Cluster) extends Actor {
+  val session = cluster.connect(Keyspaces.akkaCassandra)
+  val countAll = session.prepare("select count(*) from tweets;")
+
+  import scala.collection.JavaConversions._
+
+  def buildTweet(r: Row): Tweet = {
+    val id = r.getString("key")
+    val user = r.getString("user_user")
+    val text = r.getString("text")
+    val createdAt = r.getDate("createdat")
+    Tweet(id, user, text, createdAt)
+  }
+
+  def receive: Actor.Receive = {
+    case CountAll         =>
+      sender ! session.execute(countAll.bind()).one().getLong(0)
+    case FindAll(maximum) =>
+      val query = QueryBuilder.select().all().from(Keyspaces.akkaCassandra, "tweets").limit(maximum)
+      sender ! session.execute(query).all().map(buildTweet).toList
+
+  }
+}
