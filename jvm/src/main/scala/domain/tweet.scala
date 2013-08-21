6beb82235e79292819489e69c5e15c2fc1dffacb
Jan Machacek <jan.machacek@gmail.com>
2013-08-21 14:55:51 +0100

    Tweets

--- jvm/src/main/scala/domain/tweet.scala
+++ jvm/src/main/scala/domain/tweet.scala
@@ -1,3 +1,11 @@
 package domain
 
 import java.util.Date
+
+case class TweetId(id: String) extends AnyVal
+
+case class User(user: String) extends AnyVal
+
+case class Text(text: String) extends AnyVal
+
+case class Tweet(id: TweetId, user: User, text: Text, createdAt: Date)
