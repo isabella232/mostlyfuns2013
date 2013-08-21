0e9a751fa2a9469cc6dd4e58873aa4bf9ad83e46
Jan Machacek <jan.machacek@gmail.com>
2013-08-21 14:57:11 +0100

    Main

--- jvm/src/main/scala/core/Main.scala
+++ jvm/src/main/scala/core/Main.scala
@@ -2,15 +2,28 @@ package core
 
 import akka.actor.{Props, ActorSystem}
 import scala.annotation.tailrec
+import core.TweetReaderActor.{FindAll, CountAll}
 
-object Main extends App {
+object Main extends App with ConfigCassandraCluster {
   import Commands._
   import akka.actor.ActorDSL._
+
+  implicit lazy val system = ActorSystem()
+  val write = system.actorOf(Props(new TweetWriterActor(cluster)))
+  val read = system.actorOf(Props(new TweetReaderActor(cluster)))
+  implicit val _ = actor(new Act {
+    become {
+      case x => println(">>> " + x)
+    }
+  })
 
   @tailrec
   private def commandLoop(): Unit = {
     Console.readLine() match {
       case QuitCommand                => return
+
+      case ListCommand(count)         => read ! FindAll(count.toInt)
+      case CountCommand               => read ! CountAll
 
       case _                          => println("WTF??!!")
     }
@@ -21,6 +34,7 @@ object Main extends App {
   // start processing the commands
   commandLoop()
 
+  system.shutdown()
 }
 
 /**
